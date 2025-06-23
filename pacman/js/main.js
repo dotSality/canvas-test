98e52c8fe7a4e8df4260373adbf575b2ea60f5b6
dotSality <sality33@gmail.com>
2025-06-23 23:47:38 +0200

    added food eating logic for pacman (need some refactor), added empty files for life automate

--- pacman/js/main.js
+++ pacman/js/main.js
@@ -7,17 +7,19 @@ class GameGridCell extends GridCell {
 
   fill(child) {
     this.child = child;
-    if (!this.child) console.error('No child fo filling has been provided');
+    if (!this.child) console.error("No child fo filling has been provided");
 
     this.child.create();
   }
 
   empty() {
-    if (!this.child) console.error('No child fo filling has been provided');
+    if (!this.child) console.error("No child fo filling has been provided");
 
     this.child.disassemble();
   }
 }
+
+const PACMAN_GRID_SIZE = 20;
 
 const field = document.getElementById("field");
 const fieldContext = field.getContext("2d");
@@ -28,27 +30,12 @@ fieldContext.fillStyle = null;
 
 const { width, height } = fieldContext.canvas;
 
-const grid = generateGrid(width, height, GameGridCell, 20);
+const grid = generateGrid(width, height, GameGridCell, PACMAN_GRID_SIZE);
 
 const paintCell = (cell) => {
-  const {x, y} = cell.pivot;
+  const { x, y } = cell.pivot;
   cell.fill(new Food(x, y, 2, fieldContext));
 };
-
-// grid.traverse(paintCell);
-
-// let col = 0;
-// let r = 0;
-// setInterval(() => {
-//   if (r >= grid.size.y)  {
-//     col += 1;
-//     r = 0;
-//   }
-//   const row = grid.instance.at(col);
-//   const cell = row[r];
-//   cell.empty();
-//   r += 1;
-// }, 50)
 
 const models = document.getElementById("models");
 const modelsContext = models.getContext("2d");
@@ -58,28 +45,39 @@ const playerRowIndex = random(0, grid.size.y);
 
 const cell = grid.instance.at(playerColumnIndex).at(playerRowIndex);
 
-const { x, y } =  cell.pivot;
+const { x, y } = cell.pivot;
 
-const player = new Player(x, y, 20, 30, modelsContext);
+const player = new Player(x, y, PACMAN_GRID_SIZE, 30, modelsContext, random(0, 3));
 cell.fill(player);
 
-
-
 const render = () => {
+  const { x, y, direction, hitBox } = player;
+  let point;
+
+  if (direction === DIRECTION.Right || direction === DIRECTION.Left) {
+    const pointX = direction === DIRECTION.Left ? hitBox.x1 : hitBox.x2;
+    point = { pointX, pointY: y };
+  } else if (direction === DIRECTION.Up || direction === DIRECTION.Down) {
+    const pointY = direction === DIRECTION.Up ? hitBox.y1 : hitBox.y2;
+    point = { pointY, pointX: x };
+  }
+  const { pointX, pointY } = point;
+
+  const column = grid.instance.at(Math.floor(pointX / PACMAN_GRID_SIZE));
+  const cell = column.at(Math.floor(pointY / PACMAN_GRID_SIZE));
+
+  if (cell?.child instanceof Food && !cell.child.disassembled) {
+    const { hitBox: foodHitBox } = cell.child;
+    if ((foodHitBox.x2 >= pointX || foodHitBox.x1 <= pointX) || (foodHitBox.y1 <= pointY || foodHitBox.y2 >= pointY)) {
+      cell.empty();
+    }
+  }
   player.render();
   requestAnimationFrame(render);
-}
-
+};
 
 grid.traverse((cell) => {
   if ([player.x].includes(cell.pivot.x) && [player.y].includes(cell.pivot.y)) return;
   paintCell(cell);
 });
 render();
-// let percentage = 0;
-// let percentageDelta = 1;
-// setInterval(() => {
-//   if (percentage > 10 || percentage < 0) percentageDelta *= -1;
-//
-//   percentage += percentageDelta;
-// }, 100)
--- pacman/js/player.js
+++ pacman/js/player.js
@@ -1,128 +1,133 @@
 const degToRad = (deg) => (deg * Math.PI) / 180;
 const radToDeg = (rad) => (rad * 180) / Math.PI;
 
+const VELOCITY = 0.5;
+
 const isCollided = (hitBox, boundary, direction) => {
-    const collidedBottom = hitBox.y2 >= boundary.y && direction === DIRECTION.Down;
-    const collidedTop = hitBox.y1 <= 0 && direction === DIRECTION.Up;
-    const collidedLeft = hitBox.x1 <= 0 && direction === DIRECTION.Left;
-    const collidedRight = hitBox.x2 >= boundary.x && direction === DIRECTION.Right;
+  const collidedBottom = hitBox.y2 >= boundary.y && direction === DIRECTION.Down;
+  const collidedTop = hitBox.y1 <= 0 && direction === DIRECTION.Up;
+  const collidedLeft = hitBox.x1 <= 0 && direction === DIRECTION.Left;
+  const collidedRight = hitBox.x2 >= boundary.x && direction === DIRECTION.Right;
 
-    const deltaUp = collidedTop ? 0 : -1;
-    const deltaDown = collidedBottom ? 0 : 1;
-    const deltaRight = collidedRight ? 0 : 1;
-    const deltaLeft = collidedLeft ? 0 : -1;
+  const deltaUp = collidedTop ? 0 : -VELOCITY;
+  const deltaDown = collidedBottom ? 0 : VELOCITY;
+  const deltaRight = collidedRight ? 0 : VELOCITY;
+  const deltaLeft = collidedLeft ? 0 : -VELOCITY;
 
-    const isHorizontal = direction === DIRECTION.Left || direction === DIRECTION.Right;
-    const isVertical = direction === DIRECTION.Up || direction === DIRECTION.Down;
+  const isHorizontal = direction === DIRECTION.Left || direction === DIRECTION.Right;
+  const isVertical = direction === DIRECTION.Up || direction === DIRECTION.Down;
 
-    const deltaX = isHorizontal ? (direction === DIRECTION.Left ? deltaLeft : deltaRight) : 0;
-    const deltaY = isVertical ? (direction === DIRECTION.Up ? deltaUp : deltaDown) : 0;
+  const deltaX = isHorizontal ? (direction === DIRECTION.Left ? deltaLeft : deltaRight) : 0;
+  const deltaY = isVertical ? (direction === DIRECTION.Up ? deltaUp : deltaDown) : 0;
 
-    return { deltaX, deltaY };
-}
+  return { deltaX, deltaY };
+};
 
 const ROTATIONS = {
-    ArrowRight: DIRECTION.Right,
-    ArrowDown: DIRECTION.Down,
-    ArrowLeft: DIRECTION.Left,
-    ArrowUp: DIRECTION.Up,
-}
+  ArrowRight: DIRECTION.Right,
+  ArrowDown: DIRECTION.Down,
+  ArrowLeft: DIRECTION.Left,
+  ArrowUp: DIRECTION.Up,
+};
 
 class Player {
-    x;
-    y;
-    r;
-    alive;
-    opening;
-    ctx;
-    #openingDelta = 1;
-    direction;
+  x;
+  y;
+  r;
+  alive;
+  opening;
+  ctx;
+  #openingDelta = 1;
+  direction;
 
-    constructor(x, y, size, opening, ctx, direction = DIRECTION.Up) {
-        this.x = x;
-        this.y = y;
-        this.r = size / 2;
-        this.ctx = ctx;
-        this.opening = opening;
-        this.alive = true;
-        this.direction = direction;
+  constructor(x, y, size, opening, ctx, direction = DIRECTION.Up) {
+    this.x = x;
+    this.y = y;
+    this.r = size / 2;
+    this.ctx = ctx;
+    this.opening = opening;
+    this.alive = true;
+    this.direction = direction;
+  }
+
+  get hitBox() {
+    return getHitBox(this.x, this.y, this.r * 2);
+  }
+
+  paint(opening, erase = false) {
+    const angle = Math.PI * (opening / 100);
+
+    const rotationAngle = (this.direction * Math.PI * 2) / 4;
+
+    this.ctx.fillStyle = "yellow";
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.x, this.y);
+    this.ctx.arc(
+      this.x,
+      this.y,
+      this.r,
+      angle + rotationAngle,
+      2 * Math.PI - angle + rotationAngle,
+      erase
+    );
+    this.ctx.closePath();
+    this.ctx.fill();
+  }
+
+  create() {
+    this.paint(0);
+
+    this.ctx.globalCompositeOperation = "destination-out";
+
+    this.paint(this.opening, true);
+
+    this.ctx.globalCompositeOperation = "source-out";
+
+    this.initControls();
+  }
+
+  animate() {
+    if (this.opening <= 0 || this.opening >= 30) this.#openingDelta *= -1;
+    this.opening += this.#openingDelta;
+
+    this.ctx.clearRect(this.x - this.r, this.y - this.r, this.r * 2, this.r * 2);
+
+    this.paint(0);
+    this.ctx.globalCompositeOperation = "destination-out";
+
+    this.paint(this.opening, true);
+
+    this.ctx.globalCompositeOperation = "source-out";
+  }
+
+  disassemble() {
+    window.removeEventListener("keydown", this.keyHandler);
+  }
+
+  move() {
+    const boundary = { x: this.ctx.canvas.width, y: this.ctx.canvas.height };
+    const { deltaX, deltaY } = isCollided(this.hitBox, boundary, this.direction);
+    this.x += deltaX;
+    this.y += deltaY;
+  }
+
+  rotate(direction) {
+    this.direction = direction;
+  }
+
+  keyHandler = (event) => {
+    const key = event.key
+    if (Object.keys(ROTATIONS).includes(key)) {
+      this.rotate(ROTATIONS[key]);
     }
+  }
 
-    get hitBox() {
-        return getHitBox(this.x, this.y, this.r * 2);
-    }
+  initControls() {
+    window.addEventListener("keydown", this.keyHandler);
+  }
 
-    paint(opening, erase = false) {
-        const angle = Math.PI * (opening / 100);
-
-        const rotationAngle = (this.direction * Math.PI * 2) / 4;
-
-        this.ctx.fillStyle = "yellow";
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.x, this.y);
-        this.ctx.arc(
-            this.x,
-            this.y,
-            this.r,
-            angle + rotationAngle,
-            2 * Math.PI - angle + rotationAngle,
-            erase
-        );
-        this.ctx.closePath();
-        this.ctx.fill();
-    }
-
-    create() {
-        this.paint(0);
-
-        this.ctx.globalCompositeOperation = "destination-out";
-
-        this.paint(this.opening, true);
-
-        this.ctx.globalCompositeOperation = "source-out";
-
-        this.initControls();
-    }
-
-    animate() {
-        if (this.opening <= 0 || this.opening >= 30) this.#openingDelta *= -1;
-        this.opening += this.#openingDelta;
-
-        this.ctx.clearRect(this.x - this.r, this.y - this.r, this.r * 2, this.r * 2);
-
-        this.paint(0);
-        this.ctx.globalCompositeOperation = "destination-out";
-
-        this.paint(this.opening, true);
-
-        this.ctx.globalCompositeOperation = "source-out";
-    }
-
-    disassemble() {
-
-    }
-
-    move() {
-        const boundary = { x: this.ctx.canvas.width, y: this.ctx.canvas.height };
-        const { deltaX, deltaY } = isCollided(this.hitBox, boundary, this.direction);
-        this.x += deltaX;
-        this.y += deltaY;
-    }
-
-    rotate(direction) {
-        this.direction = direction;
-    }
-
-    initControls() {
-        window.addEventListener('keydown', (e) => {
-            if (Object.keys(ROTATIONS).includes(e.key)) {
-                this.rotate(ROTATIONS[e.key]);
-            }
-        })
-    }
-
-    render() {
-        this.move();
-        this.animate();
-    }
+  render() {
+    this.move();
+    this.animate();
+  }
 }
