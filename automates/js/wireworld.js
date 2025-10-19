5c36128252c0587a5b8675764dadba64ee45b75c
dotSality <sality33@gmail.com>
2025-10-19 23:31:05 +0200

    started wireworld automate

--- automates/js/wireworld.js
+++ automates/js/wireworld.js
@@ -18,42 +18,81 @@
 
   const canvas = document.getElementById("automate");
   const ctx = canvas.getContext("2d");
-  ctx.canvas.width = 24 * CELL_SIZE;
-  ctx.canvas.height = 13 * CELL_SIZE;
 
-  const grid = [];
-  const cells = new Set();
+  // TODO: LOGIC GATES
+  // ctx.canvas.width = 24 * CELL_SIZE;
+  // ctx.canvas.height = 13 * CELL_SIZE;
+
+  ctx.canvas.width = 14 * CELL_SIZE;
+  ctx.canvas.height = 13 * CELL_SIZE;
 
   const sizeX = ctx.canvas.width / CELL_SIZE;
   const sizeY = ctx.canvas.height / CELL_SIZE;
 
-  for (let y = 0 ; y < sizeY ; y += 1) {
-    grid[y] = [];
-    for (let x = 0 ; x < sizeX ; x += 1) {
-      grid[y][x] = CellType.EMPTY;
-      ctx.fillStyle = CellColor[grid[y][x]];
-      ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-      cells.add(`${x}${SEPARATOR}${y}`);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  const parseCell = (cell) => cell.split(",").map((n) => parseInt(n));
+
+  // TODO: LOGIC GATES
+  // cells.forEach((cell) => {
+  //   const [sx, sy] = cell.split(SEPARATOR);
+  //   const x = Number(sx);
+  //   const y = Number(sy);
+  //   if ([2, 4, 8, 10].includes(y)) {
+  //     if ([3, 4, 5, 6, 7, 8, 9, 10].includes(x)) {
+  //       ctx.fillStyle = CellColor[CellType.CONDUCTOR];
+  //       ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  //     } else if (x === 17 && [4, 8].includes(y)) {
+  //       ctx.fillStyle = CellColor[CellType.CONDUCTOR];
+  //       ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  //     }
+  //   } else if ([3, 9].includes(y)) {
+  //     if ([2, 11, 12, 13, 14, 15, 16].includes(x)) {
+  //       ctx.fillStyle = CellColor[CellType.CONDUCTOR];
+  //       ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  //     }
+  //   }
+  // });
+
+  const usableCells = new Map();
+  ["0,3", "1,3", "2,3", "3,3", "4,3", "5,3", "6,3", "8,3", "9,3", "10,3", "11,3", "12,3", "13,3", "0,9", "1,9", "2,9",
+    "3,9", "4,9", "5,9", "7,9", "8,9", "9,9", "10,9", "11,9", "12,9", "13,9", "6,2", "7,2", "6,4", "7,4", "6,8", "7,8", "6,10", "7,10"]
+    .forEach((coords) => {
+      usableCells.set(coords, CellType.CONDUCTOR);
+    });
+
+  const isConverted = (neighbours) => {
+    let count = 0;
+
+    for (const neighbour of neighbours) {
+      if (count > 2) break;
+      if (!neighbour) continue;
+      const type = usableCells.get(neighbour);
+      count += type === CellType.ELECTRON_HEAD ? 1 : 0;
     }
-  }
+
+    return count === 1 || count === 2;
+  };
 
   const getNeighbours = (x, y) => {
     return [
       [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
       [x - 1, y], /* [x, y] */ [x + 1, y],
       [x - 1, y + 1], [x, y + 1], [x + 1, y + 1],
-    ].filter(([x, y]) => x >= 0 && y >= 0 && x < sizeX && y < sizeY);
+    ]
+      .reduce((acc, coords) => {
+        const [x, y] = coords;
+        const cell = usableCells.get(`${x},${y}`);
+        if (cell) {
+          acc.push(cell);
+        }
+        return acc;
+      }, []);
   };
 
-  const isConverted = (neighbours) => {
-    if (![1, 2].includes(neighbours.length)) return false;
-
-    return neighbours.every((neighbour) => neighbour === CellType.ELECTRON_HEAD);
-  };
-
-  const cellNextGeneration = (grid, x, y) => {
-    const cell = grid[x][y];
-    switch (cell) {
+  const cellNextGeneration = (coords, type) => {
+    const [x, y] = parseCell(coords);
+    switch (type) {
       case CellType.EMPTY:
         return CellType.EMPTY;
       case CellType.ELECTRON_HEAD:
@@ -66,27 +105,41 @@
     }
   };
 
-  cells.forEach((cell) => {
-    const [sx, sy] = cell.split(SEPARATOR);
-    const x = Number(sx);
-    const y = Number(sy);
-    if ([2, 4, 8, 10].includes(y)) {
-      if ([3, 4, 5, 6, 7, 8, 9, 10].includes(x)) {
-        ctx.fillStyle = CellColor[CellType.CONDUCTOR];
-        ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-      } else if (x === 17 && [4, 8].includes(y)) {
-        ctx.fillStyle = CellColor[CellType.CONDUCTOR];
-        ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-      }
-    } else if ([3, 9].includes(y)) {
-      if ([2, 11, 12, 13, 14, 15, 16].includes(x)) {
-        ctx.fillStyle = CellColor[CellType.CONDUCTOR];
-        ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-      }
-    }
+  ["0,3", "0,9"].forEach((coords) => {
+    usableCells.set(coords, CellType.ELECTRON_HEAD);
   });
 
-  const simulateNextGeneration = (activeCells) => {
-
+  /**
+   * @param {Map<String, Number>} cells
+   */
+  const renderCells = (cells) => {
+    cells.entries().forEach(([coords, type]) => {
+      const [x, y] = parseCell(coords);
+      ctx.fillStyle = CellColor[type];
+      ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    });
   };
+
+  renderCells(usableCells);
+
+  /**
+   * @param {Map<String, Number>} activeCells
+   * @returns {Map<String, Number>}
+   */
+  const simulateNextGeneration = (activeCells) => {
+    const newCells = new Map();
+    activeCells.entries().forEach(([coords, oldType]) => {
+      const newType = cellNextGeneration(coords, oldType);
+      newCells.set(coords, newType);
+    });
+    console.log(newCells);
+    return newCells;
+  };
+
+  const render = () => {
+    const newCells = simulateNextGeneration(usableCells);
+    renderCells(newCells);
+    // requestAnimationFrame(render);
+  };
+  render();
 })();
